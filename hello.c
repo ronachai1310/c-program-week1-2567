9fd860baf2721ae5232fa62f4983bf2720cad24d
ronachai1310 <rnchayphuththrasu526@gmail.com>
2024-05-27 12:35:42 +0700

    first commit

---
+++ hello.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+void main(){
+    //Create integer variable
+    int age = 15;
+    //print Hello World to the screen
+    printf("Hello World! \n");
+    printf("My name is Mr.Ronachai \n");
+    printf("Age: %d\n",age);
+    return 0;
+}
